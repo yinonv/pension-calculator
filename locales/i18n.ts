5d776fffd59d7ebe370dd7d9cefe6a4c01bddc76
Yinon Vahab <yinonv@gmail.com>
2020-07-12 23:38:45 +0300

    added icons

--- locales/i18n.ts
+++ locales/i18n.ts
@@ -1,3 +1,4 @@
+import {I18nManager} from 'react-native'
 import I18n from "i18n-js";
 import * as Localization from 'expo-localization';
 
@@ -5,6 +6,8 @@ import en from "./en";
 import he from "./he";
 
 I18n.locale = Localization.locale;
+I18nManager.allowRTL(Localization.isRTL)
+I18nManager.forceRTL(Localization.isRTL)
 
 I18n.fallbacks = true;
 I18n.translations = {
